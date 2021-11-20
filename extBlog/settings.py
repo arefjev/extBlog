e5535a7e5801e5822dd2a5812a10e1010f1cfbcc
Egor F. Arefyev <efarefyev@gmail.com>
2021-11-21 00:09:34 +0400

    extBlog-5 - Add decouple support to store critical parameters outside project code

--- extBlog/settings.py
+++ extBlog/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY: Added decouple support to store critical information outside project
 SECRET_KEY = decouple_config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
